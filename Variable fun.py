fdcc31dc59d9e628b769394c612f21aee79172d6
MARK <markcoke0@gmail.com>
2025-01-26 23:12:56 -0500

    Add files via upload

---
+++ Variable fun.py
@@ -0,0 +1,19 @@
+# Result variables
+result1 = 5 * 6
+result2 = 8 / 3
+result3 = 8 // 3
+result4 = 8 % 3
+result5 = 4200000 + 23090
+result6 = 4200000 - 7633
+name = "Mark Coke"
+
+# Print results
+print(f"The answer to your query is: {result1}")
+print(f"The answer to your query is: {result2}")
+print(f"The answer to your query is: {result3}")
+print(f"The answer to your query is: {result4}")
+print(f"The answer to your query is: {result5}")
+print(f"The answer to your query is: {result6}")
+
+# Print name
+print(name)
